dc638909d29ba402e4a617db2b023f4a5a2239b2
Heike Rolker <78981888+heikeehee@users.noreply.github.com>
2026-04-27 06:23:05 +0200

    Updated packages.R

--- 01-smallholder-material-flow/scripts/packages.R
+++ 01-smallholder-material-flow/scripts/packages.R
@@ -1,128 +1,26 @@
-# general packages needed for all chapters
-if(!require(devtools)) {
-  install.packages("devtools", repos = "http://cran.rstudio.com")
-  library(devtools)
-}
+# Packages — 01-smallholder-material-flow
+# Install and load all dependencies for this project.
+# Run once on a new machine before executing any other script.
 
-if(!require(bookdown)){
-  devtools::install_github("rstudio/bookdown")
-  library(bookdown)
-}
+packages_cran <- c(
+  # Core
+  "tidyverse", "data.table", "here", "purrr",
+  # Data import
+  "haven", "readxl",
+  # Tables
+  "knitr", "kableExtra",
+  # Visualisation
+  "ggplot2", "ggimage", "ggthemes", "treemapify",
+  "plotly", "RColorBrewer", "viridis", "hrbrthemes",
+  # Reporting
+  "bookdown", "tinytex",
+  # Utilities
+  "Hmisc", "stringi", "stringr", "pander", "devtools"
+)
 
-if(!require(thesisdown)){
-  devtools::install_github("ismayc/thesisdown")
-  library(thesisdown)
-}
-
-if(!require(kableExtra)){
-  devtools::install_github("haozhu233/kableExtra")
-  library(kableExtra)
-}
-
-if(!require(tinytex)){
-  devtools::install_github('yihui/tinytex')
-  library(tinytex)
-  options(tinytex.verbose = TRUE)
-}
-
-if(!require(flextable)){
-  devtools::install_github("davidgohel/flextable")
-  library(flextable)
-}
-
-if(!require(tidyverse)){
-  install.packages("tidyverse")
-  library(tidyverse)
-}
-
-if(!require(ggplot2)){
-  install.packages("ggplot2")
-  library(ggplot2)
-}
-
-if(!require(knitr)){
-  install.packages("knitr")
-  library(knitr)
-}
-
-if(!require(dplyr)){
-  install.packages("dplyr")
-  library(dplyr)
-}
-
-if(!require(data.table)){
-  install.packages("data.table")
-  library(data.table)
-}
-
-if(!require(Hmisc)){
-  install.packages("Hmisc")
-  library(Hmisc)
-}
-
-if(!require(stringi)){
-  install.packages("stringi")
-  library(stringi)
-}
-
-if(!require(stringr)){
-  install.packages("stringr")
-  library(stringr)
-}
-
-if(!require(haven)){
-  install.packages("haven")
-  library(haven)
-}
-
-if(!require(plotly)){
-  install.packages("plotly")
-  library(plotly)
-}
-
-if(!require(readxl)){
-  install.packages("readxl")
-  library(readxl)
-}
-
-if(!require(pander)){
-  install.packages("pander")
-  library(pander)
-}
-
-if(!require(RColorBrewer)){
-  install.packages("RColorBrewer")
-  library(RColorBrewer)
-}
-if(!require(viridis)){
-  install.packages("viridis")
-  library(viridis)
-}
-if(!require(hrbrthemes)){
-  install.packages("hrbrthemes")
-  library(hrbrthemes)
-}
-if(!require(ggthemes)){
-  install.packages("ggthemes")
-  library(ggthemes)
-}
-
-if(!require(ggimage)){
-  install.packages("ggimage")
-  library(ggimage)
-}
-
-if(!require(treemapify)){
-  install.packages("treemapify")
-  library(treemapify)
-}
-
-if(!require(purrr)){
-  install.packages("purrr")
-  library(purrr)
-}
-
-if(!require(here)){
-  install.packages("here")
-  library(here)
+for (pkg in packages_cran) {
+  if (!require(pkg, character.only = TRUE)) {
+    install.packages(pkg)
+    library(pkg, character.only = TRUE)
+  }
 }
